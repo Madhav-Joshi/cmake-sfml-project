694df95b6fcd8f3bb39e45179afaec7c1ab9764b
MaddyIITB <madhavjoshi@iitb.ac.in>
2025-02-24 21:42:27 +0530

    feat: basic organisation and basic animation, yay

---
+++ src/configuration.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace conf
+{
+    // Window configuration
+    sf::Vector2u const window_size{1920u, 1080u};
+    sf::Vector2f const window_size_f = static_cast<sf::Vector2f>(window_size); // Convert to float
+    uint32_t const max_framerate = 60;
+    float const dt = 1.0f / static_cast<float>(max_framerate);
+
+    // Star configuration
+    uint32_t const count = 1000;
+    float const radius = 20.0f;
+    float const far = 10.0f;
+    float const near = 1.0f;
+    float const speed = 1.0f;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,61 @@
 #include <SFML/Graphics.hpp>
+#include <random>
+#include "events.hpp"
+#include "configuration.hpp"
+#include "star.hpp"
+
+std::vector<Star> createStars(uint32_t count)
+{
+    std::vector<Star> stars;
+    stars.reserve(count);
+
+    // Random numbers generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+
+    // Create randomly distributed stars on the screen
+    for (uint32_t i{count}; i > 0; --i)
+    {
+        float const x = (dis(gen) - 0.5f) * conf::window_size_f.x;
+        float const y = (dis(gen) - 0.5f) * conf::window_size_f.y;
+        float const z = dis(gen) * (conf::far - conf::near) + conf::near;
+        
+        stars.push_back({{x, y}, z});
+    }
+
+    return stars;
+}
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    auto window = sf::RenderWindow{{conf::window_size.x, conf::window_size.y}, "CMake SFML Project",sf::Style::Fullscreen};
+    window.setFramerateLimit(conf::max_framerate);
+
+    std::vector<Star> stars = createStars(conf::count);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        processEvents(window);
+
+        // Fake travel toward increasing Z
+        for (auto& s : stars)
         {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+            s.z -= conf::speed * conf::dt;
         }
 
         window.clear();
-        
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color::Green);
-        window.draw(shape);
+
+        sf::CircleShape shape{conf::radius};
+        shape.setOrigin(conf::radius, conf::radius);
+
+        for(auto const& s : stars)
+        {
+            float const scale = 1.0f / s.z;
+            shape.setPosition(s.position * scale + conf::window_size_f * 0.5f);
+            shape.setScale(scale, scale);
+            window.draw(shape);
+        }
 
         window.display();
     }
